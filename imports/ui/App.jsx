02a619103bc72a06ba73c4d8520b379c78fbd18e
Mariano Hurtado de Mendoza Carranza <theunfactoringguru@gmail.com>
2022-05-03 00:46:53 -0500

    added tailwind css

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
-
+import { Header } from './Header';
+import '../../client/main.css';
 //Contact: name, email, imageURL
 export const App = () => (
   <div>
-    <h1>Meteor Wallet - Galaxy</h1>
-    <ContactForm/>
-    <ContactList/>
+    <Header/>
+    <div className='min-h-full'>
+      <div className='max-w-4xl mx-auto p-2'>
+        <ContactForm/>
+        <ContactList/>
+      </div>
+    </div>
   </div>
 );
